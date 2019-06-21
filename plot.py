453a72f6d271975bf973d0d5d12d59f60abec483
Noah Hummel <n.p.hummel@gmail.com>
2019-06-21 23:32:20 +0200

    [FEATURE] color events differently in plot

--- plot.py
+++ plot.py
@@ -12,10 +12,10 @@ def plot_ticks(ticks):
 	x_decay = [t["time"] for t in prune_ticks(ticks) if any([e["id"] == "decay" for e in t["events"]])]
 	y_decay = [t["score"] for t in prune_ticks(ticks) if any([e["id"] == "decay" for e in t["events"]])]
 
-	plt.plot(x, y)
-	plt.scatter(x_int_leet, y_int_leet, 33, c="r", alpha=0.66, marker=r'$\Delta$', label="Interrupted")
-	plt.scatter(x_leet_timing, y_leet_timing, 33, c="r", alpha=0.33, marker=r'$\Delta$', label="Mistimed")
-	plt.scatter(x_decay, y_decay, 33, c="g", alpha=0.5, marker=r'$\nabla$', label="Decay")
+	plt.plot(x, y, c='k')
+	plt.scatter(x_int_leet, y_int_leet, 33, c="r", alpha=1, marker=r'$\Delta$', label="Interrupted")
+	plt.scatter(x_leet_timing, y_leet_timing, 33, c="b", alpha=1, marker=r'$\Delta$', label="Mistimed")
+	plt.scatter(x_decay, y_decay, 33, c="g", alpha=0.2, marker=r'$\nabla$', label="Decay")
 	
 	plt.grid(True)
 	plt.ylabel("Score")
