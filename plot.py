2f9eb017e0b86c19170889fa72220c24463eead4
Noah Hummel <n.p.hummel@gmail.com>
2019-06-22 17:43:38 +0200

    [REFACTOR] clean up plot.py

--- plot.py
+++ plot.py
@@ -4,24 +4,40 @@ import numpy as np
 from simulate import prune_ticks
 
 def plot_ticks(ticks):
-	x, y = [t["time"] for t in ticks], [t["score"] for t in ticks]
-	x_int_leet = [t["time"] for t in prune_ticks(ticks) if any([e["id"] == "interrupted_leet" for e in t["events"]])]
-	y_int_leet = [t["score"] for t in prune_ticks(ticks) if any([e["id"] == "interrupted_leet" for e in t["events"]])]
-	x_leet_timing = [t["time"] for t in prune_ticks(ticks) if any([e["id"] == "incorrectly_timed_leet" for e in t["events"]])]
-	y_leet_timing = [t["score"] for t in prune_ticks(ticks) if any([e["id"] == "incorrectly_timed_leet" for e in t["events"]])]
-	x_decay = [t["time"] for t in prune_ticks(ticks) if any([e["id"] == "decay" for e in t["events"]])]
-	y_decay = [t["score"] for t in prune_ticks(ticks) if any([e["id"] == "decay" for e in t["events"]])]
+    """
+    Takes a series of ticks (not pruned!) and plots them.
+    """
+    eventful_ticks = list(prune_ticks(ticks))
 
-	plt.plot(x, y, c='k')
-	plt.scatter(x_int_leet, y_int_leet, 33, c="r", alpha=1, marker=r'$\Delta$', label="Interrupted")
-	plt.scatter(x_leet_timing, y_leet_timing, 33, c="b", alpha=1, marker=r'$\Delta$', label="Mistimed")
-	plt.scatter(x_decay, y_decay, 33, c="g", alpha=0.2, marker=r'$\nabla$', label="Decay")
-	
-	plt.grid(True)
-	plt.ylabel("Score")
-	plt.xlabel("Time [d]")
-	start, end = plt.xlim()
-	xtick_count = int(end) // 1440
+    def tick_contains_event(tick, event_name):
+        return any([e["id"] == event_name for e in tick["events"]])
 
-	plt.xticks(np.arange(0, end, 1440), range(xtick_count))
-	plt.show()
+    # score vs time
+    x, y = [t["time"] for t in ticks], [t["score"] for t in ticks]
+
+    # data points for interrupted_leet event
+    x_int_leet = [t["time"] for t in eventful_ticks if tick_contains_event(t, "interrupted_leet")]
+    y_int_leet = [t["score"] for t in eventful_ticks if tick_contains_event(t, "interrupted_leet")]
+
+    # data points for incorrectly_timed_leet event
+    x_leet_timing = [t["time"] for t in eventful_ticks if tick_contains_event(t, "incorrectly_timed_leet")]
+    y_leet_timing = [t["score"] for t in eventful_ticks if tick_contains_event(t, "incorrectly_timed_leet")]
+
+    # data points for decay event
+    x_decay = [t["time"] for t in eventful_ticks if tick_contains_event(t, "decay")]
+    y_decay = [t["score"] for t in eventful_ticks if tick_contains_event(t, "decay")]
+
+    plt.plot(x, y, c='k')
+    plt.scatter(x_int_leet, y_int_leet, 33, c="r", alpha=1, marker=r'$\Delta$', label="Interrupted")
+    plt.scatter(x_leet_timing, y_leet_timing, 33, c="b", alpha=1, marker=r'$\Delta$', label="Mistimed")
+    plt.scatter(x_decay, y_decay, 33, c="g", alpha=0.2, marker=r'$\nabla$', label="Decay")
+
+    plt.grid(True)
+    plt.ylabel("Score")
+    plt.xlabel("Time [d]")
+
+    start, end = plt.xlim()
+    xtick_count = int(end) // 1440  # one xtick per day
+    plt.xticks(np.arange(0, end, 1440), range(xtick_count))
+
+    plt.show()
